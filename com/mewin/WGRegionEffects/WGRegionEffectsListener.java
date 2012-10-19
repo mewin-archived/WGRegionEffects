f37754e301b36da04baafaf3d54af433f7f64480
BangL <henno.rickowski@googlemail.com>
2012-10-19 20:06:11 +0200

    cleanup

--- com/mewin/WGRegionEffects/WGRegionEffectsListener.java
+++ com/mewin/WGRegionEffects/WGRegionEffectsListener.java
@@ -14,12 +14,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
--- com/mewin/WGRegionEffects/WGRegionEffectsPlugin.java
+++ com/mewin/WGRegionEffects/WGRegionEffectsPlugin.java
@@ -19,8 +19,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
