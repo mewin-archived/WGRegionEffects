27b5917617615d9a70d6a49743f23bd0badfedf7
mewin <>
2012-10-24 23:40:29 +0200

    moved getFlags method to new Util class

--- com/mewin/WGRegionEffects/WGRegionEffectsListener.java
+++ com/mewin/WGRegionEffects/WGRegionEffectsListener.java
@@ -4,20 +4,13 @@
  */
 package com.mewin.WGRegionEffects;
 
-import com.mewin.WGRegionEffects.flags.PotionEffectDesc;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.ConcurrentModificationException;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -49,65 +42,20 @@ public class WGRegionEffectsListener implements Listener {
     }
     
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent e)
+    public synchronized void onPlayerDeath(PlayerDeathEvent e)
     {
         WGRegionEffectsPlugin.playerEffects.remove(e.getEntity());
     }
     
-    
-    private void updatePlayerEffects(Player p)
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e)
     {
-        RegionManager rm = wgPlugin.getRegionManager(p.getWorld());
-        if (rm == null)
-        {
-            return ;
-        }
-        ApplicableRegionSet regions = rm.getApplicableRegions(p.getLocation());
-        
-        List<PotionEffectDesc> effects = new ArrayList<>();
-        Iterator<ProtectedRegion> itr = regions.iterator();
-        
-        try
-        {
-            Field f = regions.getClass().getDeclaredField("globalRegion");
-            f.setAccessible(true);
-            ProtectedRegion region = (ProtectedRegion) f.get(regions);
-            
-            Set<PotionEffectDesc> flags = (Set<PotionEffectDesc>) region.getFlag(WGRegionEffectsPlugin.EFFECT_FLAG);
-            Iterator<PotionEffectDesc> itr2 = flags.iterator();
-            
-            while(itr2.hasNext())
-            {
-                PotionEffectDesc effect = itr2.next();
-                
-                effects.add(effect);
-            }
-        }
-        catch(NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException | NullPointerException ex)
-        {
-            
-        }
-        if (!p.isDead())
-        {
-            while(itr.hasNext())
-            {
-                ProtectedRegion region = itr.next();
-
-                Set<PotionEffectDesc> flags = (Set<PotionEffectDesc>) region.getFlag(WGRegionEffectsPlugin.EFFECT_FLAG);
-                if (flags == null)
-                {
-                    continue;
-                }
-                Iterator<PotionEffectDesc> itr2 = flags.iterator();
-
-                while(itr2.hasNext())
-                {
-                    PotionEffectDesc effect = itr2.next();
-
-                    effects.add(effect);
-                }
-            }
-        }
-        WGRegionEffectsPlugin.playerEffects.put(p, effects);
+        updatePlayerEffects(e.getPlayer());
+    }
+    
+    
+    private synchronized void updatePlayerEffects(Player p)
+    {
+        WGRegionEffectsPlugin.playerEffects.put(p, Util.getEffectsForLocation(wgPlugin, p.getLocation())); 
     }
 }
